cc0eb0db8dc714cbaa1511ea40bcafe19939dc9d
lonevetad <ottins1995@gmail.com>
2020-07-10 10:38:55 +0200

    Corrected bug upo adding in MapTreeAVL-MinIter

---
+++ JavaBackup/AllProjects/src/tests/tDataStruct/TAVLMinMax.java
@@ -0,0 +1,32 @@
+package tests.tDataStruct;
+
+import dataStructures.MapTreeAVL;
+import tools.Comparators;
+
+public class TAVLMinMax {
+
+	public static void main(String[] args) {
+		MapTreeAVL<Integer, Integer> t;
+		Integer x;
+		t = MapTreeAVL.newMap(MapTreeAVL.Optimizations.MinMaxIndexIteration, Comparators.INTEGER_COMPARATOR);
+		for (int i = 0; i < 10; i++) {
+			x = i * 10;
+			t.put(x, x);
+		}
+		pr(t);
+		x = 5;
+		System.out.println("adding " + x);
+		t.put(x, x);
+		pr(t);
+		x = 85;
+		System.out.println("adding " + x);
+		t.put(x, x);
+		pr(t);
+	}
+
+	static void pr(MapTreeAVL<Integer, Integer> t) {
+		System.out.println("\n--------");
+		System.out.println(t);
+		t.forEach((k, v) -> System.out.println("k: " + k));
+	}
+}
