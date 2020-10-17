06033f05b0e42ce7296e14fa77746506535e6e53
lonevetad <ottins1995@gmail.com>
2020-10-18 01:49:37 +0200

    ...

---
+++ JavaBackup/AllProjects/src/grammars/grammarV2/RuleElementSingle.java
@@ -0,0 +1,8 @@
+package grammars.grammarV2;
+
+/** Marker-class */
+public class RuleElementSingle extends RuleElement {
+	private static final long serialVersionUID = -3621741255050L;
+
+	public RuleElementSingle(GrammarElement firstElement) { super(firstElement); }
+}
