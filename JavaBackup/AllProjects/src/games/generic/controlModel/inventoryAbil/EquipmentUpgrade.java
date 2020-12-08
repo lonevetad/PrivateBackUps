b3f882fbc8f395a8a01c5d5f9a0084512fb2c81e
Marco Ottina <ottins1995@gmail.com>
2020-12-08 11:43:34 +0100

    Added new ability: more life regen paying in precision depending on l… (#51)
    * Added new ability: more life regen paying in precision depending on life missing
    
    * Added description to equip upgrades, added some upgrades gem/tree/color based and ONE legenrady
    
    * added other 3 equip upgrades
    
    * added some other equip upgrades, changes in life bonuses from stats
    
    * Corrected an error in equipUpgrade.json, added 1 entry

--- JavaBackup/AllProjects/src/games/generic/controlModel/inventoryAbil/EquipmentUpgrade.java
+++ JavaBackup/AllProjects/src/games/generic/controlModel/inventoryAbil/EquipmentUpgrade.java
@@ -3,13 +3,18 @@ package games.generic.controlModel.inventoryAbil;
 import java.util.function.Function;
 
 /**
- * Simple extension to mark an attribute upgrade designed for
+ * Simple extension to mark an {@link AttributeUpgrade} designed for
  * {@link EquipmentItem}s.
  */
 public interface EquipmentUpgrade extends AttributesUpgrade {
 	public static final Function<EquipmentUpgrade, String> KEY_EXTRACTOR = eu -> eu.getName();
 
+	public String getDescription();
+
+	public void setDescription(String description);
+
 	public EquipmentItem getEquipmentAssigned();
 
 	public void setEquipmentAssigned(EquipmentItem equipmentAssigned);
+
 }
